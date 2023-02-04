4343930249a1e2c856d7b3741c3af1a5fa45a48f
elonehoo <hi@elonehoo.me>
2023-02-04 14:07:12 +0800

    test: update

---
+++ test/src/typescriptNormal2.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+interface Props{
+  name:string
+}
+
+defineProps<Props>()
+console.log('Hello')
+</script>
+
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+</template>
