f727c7c2f364eea9b0b1aa96bb7a9cc4850d088b
elonehoo <hi@elonehoo.me>
2023-03-04 13:06:58 +0800

    chore: add playground nuxt

---
+++ playground/nuxt/nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules:[
+    'unplugin-vue-prop/nuxt',
+  ],
+  css:[
+    '@/assets/style.css'
+  ]
+})
